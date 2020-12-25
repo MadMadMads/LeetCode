c9a032f7abcdb76945eab9d435a0abb9502ecca6
罗智 <754729961@qq.com>
2020-12-25 17:13:05 +0800

    反转链表

---
+++ src/leetcode/T92reverseBetween.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * @author: Luo
+ * @description:92. 反转链表 II
+ * @time: 2020/12/25 15:04
+ * Modified By:https://leetcode-cn.com/problems/reverse-linked-list-ii/
+ */
+public class T92reverseBetween {
+    ListNode successor = null;
+    public ListNode reverse(ListNode head,int n) {
+        if (n == 1) {
+            successor =  head.next;
+            return head;
+        }
+        ListNode last = reverse(head.next,n-1);
+        head.next.next = head;
+        head.next = successor;
+        //  last = return head;
+        return last;
+    }
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (m == 1) {
+            return reverse(head,n);
+        }
+        head.next = reverseBetween(head.next,m - 1, n -1);
+        return head;
+    }
+}
