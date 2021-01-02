ef261a9cbfbfa5d85904dc6ddd31b882e1bc8ab3
罗智 <754729961@qq.com>
2021-01-02 21:05:24 +0800

    309. 最佳买卖股票时机含冷冻期

--- src/leetcode/T121maxProfit.java
+++ src/leetcode/T121maxProfit.java
@@ -2,17 +2,28 @@ package leetcode;
 
 /**
  * @author: Luo
- * @description:121. 买卖股票的最佳时机
- * @time: 2021/1/2 20:21
- * Modified By:https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
+ * @description:122. 买卖股票的最佳时机 II
+ * @time: 2021/1/2 20:28
+ * Modified By:https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
  */
 public class T121maxProfit {
+    /** DP **/
     public int maxProfit(int[] prices) {
         int dp_i_0 = 0,dp_i_1 = Integer.MIN_VALUE;
-        for (int i = 0;i <prices.length; i++) {
+        for (int i = 0; i < prices.length; i++) {
+            dp_i_1 = Math.max(dp_i_1,dp_i_0 - prices[i]);
             dp_i_0 = Math.max(dp_i_0,dp_i_1 + prices[i]);
-            dp_i_1 = Math.max(dp_i_1,-prices[i]);
+
         }
         return dp_i_0;
+    }
+    /** 贪心，只要涨幅了就每天都卖 **/
+    public int maxProfit1(int[] prices) {
+        int res = 0;
+        for (int i = 1;i < prices.length; i++) {
+            int temp = prices[i] - prices[i - 1];
+            if (temp > 0) res += temp;
+        }
+        return res;
     }
 }
---
+++ src/leetcode/T309maxProfit.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * @author: Luo
+ * @description:309. 最佳买卖股票时机含冷冻期
+ * @time: 2021/1/2 20:39
+ * Modified By:https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+ */
+public class T309maxProfit {
+    public int maxProfit(int[] prices) {
+        int dp_i_0 = 0,dp_i_1 = Integer.MIN_VALUE,dp_i_pre_0 = 0;
+        for (int i = 0; i < prices.length; i++) {
+            int temp = dp_i_0;
+            dp_i_0 = Math.max(dp_i_0,dp_i_1 + prices[i]);
+            dp_i_1 = Math.max(dp_i_1,dp_i_pre_0 - prices[i]);
+            dp_i_pre_0 = temp;
+        }
+        return dp_i_0;
+    }
+}
