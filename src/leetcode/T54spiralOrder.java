0dece83d5eba7ed87ca15e8398b620afb2499c3a
罗智 <754729961@qq.com>
2021-01-03 19:24:54 +0800

    54. 螺旋矩阵

---
+++ src/leetcode/T54spiralOrder.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author: Luo
+ * @description:54. 螺旋矩阵
+ * @time: 2021/1/3 19:07
+ * Modified By:https://leetcode-cn.com/problems/spiral-matrix/
+ */
+public class T54spiralOrder {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> list = new ArrayList<Integer>();
+        if (matrix.length == 0) return list;
+        int m = matrix.length,n = matrix[0].length;
+        int count = (Math.min(m, n) + 1) / 2;
+        int i = 0;
+        while (i < count) {
+            for (int j = i; j < n - i;j++) list.add(matrix[i][j]);
+            for (int j = i + 1; j < m - i; j++) list.add(matrix[j][n - 1 - i]);
+            for (int j = n - 2 - i; j >= i && m - 1 - i != i; j--) list.add(matrix[m - 1 - i][j]);
+            for (int j = m - 2 - i; j > i && n - 1 - i != i ;j--) list.add(matrix[j][i]);
+            i++;
+        }
+        return list;
+    }
+}
