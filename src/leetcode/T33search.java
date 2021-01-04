5f48e41c7dea1822d4283b5180902bbd21ac5ebc
罗智 <754729961@qq.com>
2021-01-04 22:26:14 +0800

    33. 搜索旋转排序数组

--- src/leetcode/T33search.java
+++ src/leetcode/T33search.java
@@ -8,25 +8,20 @@ package leetcode;
  */
 public class T33search {
     public int search(int[] nums, int target) {
-        int lo = 0, hi = nums.length - 1, mid = 0;
-        while (lo <= hi) {
-            mid = lo + (hi - lo) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            }
-            // 先根据 nums[mid] 与 nums[lo] 的关系判断 mid 是在左段还是右段
-            if (nums[mid] >= nums[lo]) {
-                // 再判断 target 是在 mid 的左边还是右边，从而调整左右边界 lo 和 hi
-                if (target >= nums[lo] && target < nums[mid]) {
-                    hi = mid - 1;
-                } else {
-                    lo = mid + 1;
+        int left = 0,right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] >= nums[left]) {
+                if (target < nums[mid] && target >= nums[left]) right = mid - 1;
+                else {
+                    left = mid + 1;
                 }
             } else {
-                if (target > nums[mid] && target <= nums[hi]) {
-                    lo = mid + 1;
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
                 } else {
-                    hi = mid - 1;
+                    right = mid - 1;
                 }
             }
         }
